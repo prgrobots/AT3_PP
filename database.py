ed62e69203a5810712222c24556313c88221e582
prgrobots <murray.hay@nmtafe.wa.edu.au>
2024-05-22 17:44:42 +0800

    init

---
+++ database.py
@@ -0,0 +1,17 @@
+''''
+Holds the store items
+'''
+
+
+database = {
+    'green_apple': '🍏',
+    'blueberries': '🫐',
+    'watermelon': '🍉',
+    'kiwi': '🥝',
+    'pear': '🍐',
+    'carrot': '🥕',
+    'bell_pepper': '🫑',
+    'grapes': '🍇',
+    'pineapple': '🍍',
+    'potato': '🥔'
+}
